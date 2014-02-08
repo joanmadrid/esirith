6545d1b11600bf5682a51de63def58ef299c1b48
Joan Madrid <joanmadrid@gmail.com>
2014-02-08 19:50:03 +0100

    Add FOSUser bundle basic setup

---
+++ src/Game/UserBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Game\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="game_user")
+ * @ORM\Entity(repositoryClass="Game\UserBundle\Entity\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
