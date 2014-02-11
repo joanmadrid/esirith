612aa2c63d416aab19e0846ed9be2be960db054c
Joan Madrid <joanmadrid@gmail.com>
2014-02-11 22:32:26 +0100

    Add itemRender on inventory + shops

---
+++ src/Game/ItemBundle/Twig/ItemExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Game\ItemBundle\Twig;
+
+use Game\ItemBundle\Entity\Item;
+
+class ItemExtension extends \Twig_Extension
+{
+    private $environment;
+
+    /**
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('renderItem', array($this, 'renderItem')),
+        );
+    }
+
+    /**
+     * @param Item $item
+     * @return mixed
+     */
+    public function renderItem($item)
+    {
+        $class = explode("\\", get_class($item));
+        $class = array_pop($class);
+        return $this->environment->render('ItemBundle:ItemRender:'.$class.'.html.twig', array('item' => $item));
+    }
+
+    /**
+     * @param \Twig_Environment $environment
+     */
+    public function initRuntime(\Twig_Environment $environment)
+    {
+        $this->environment = $environment;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'item_extension';
+    }
+}
