95cbb275a04896dcac7805c0c7468f4e37a53b12
Joan Madrid <joanmadrid@gmail.com>
2014-02-22 15:15:58 +0100

    Add battle base + mocks

--- src/Game/BattleBundle/Manager/BattleManager.php
+++ src/Game/BattleBundle/Manager/BattleManager.php
@@ -3,7 +3,12 @@
 namespace Game\BattleBundle\Manager;
 
 
+use Game\CharacterBundle\Entity\Character;
 use Game\CoreBundle\Manager\CoreManager;
+use Game\MapBundle\Entity\Poi;
+use Game\MonsterBundle\Entity\Monster;
+use Game\BattleBundle\Entity\Battle;
+use Game\MonsterBundle\Model\MonsterGroup;
 
 class BattleManager extends CoreManager
 {
@@ -15,4 +20,19 @@ class BattleManager extends CoreManager
     {
         return parent::getRepository();
     }
+
+    /**
+     * Crea una batalla PvM
+     *
+     * @param Character $char
+     * @param MonsterGroup $monsters
+     * @param Poi $poi
+     * @return Battle
+     */
+    public function createMonsterBattle(Character $char, MonsterGroup $monsters, Poi $poi)
+    {
+        return new Battle();
+    }
+
+
 }
