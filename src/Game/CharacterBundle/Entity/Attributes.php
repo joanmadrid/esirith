5080a853f205babf1cbec622c5c459fbdadb91fe
Joan Madrid <joanmadrid@gmail.com>
2014-03-02 21:11:37 +0100

    Add spi, mana, int

--- src/Game/CharacterBundle/Entity/Attributes.php
+++ src/Game/CharacterBundle/Entity/Attributes.php
@@ -11,49 +11,77 @@ abstract class Attributes
      *
      * @ORM\Column(name="hp", type="integer")
      */
-    protected $hp;
+    protected $hp = 1;
 
     /**
      * @var int
      *
      * @ORM\Column(name="current_hp", type="integer")
      */
-    protected $currentHp;
+    protected $currentHp = 1;
 
     /**
      * @var integer
      *
      * @ORM\Column(name="damage", type="integer")
      */
-    protected $damage;
+    protected $damage = 1;
 
     /**
      * @var integer
      *
      * @ORM\Column(name="defense", type="integer")
      */
-    protected $defense;
+    protected $defense = 1;
 
     /**
      * @var integer
      *
      * @ORM\Column(name="str", type="integer")
      */
-    protected $str;
+    protected $str = 1;
 
     /**
      * @var integer
      *
      * @ORM\Column(name="dex", type="integer")
      */
-    protected $dex;
+    protected $dex = 1;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="intel", type="integer")
+     */
+    protected $int = 1;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="spi", type="integer")
+     */
+    protected $spi = 1;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="current_mana", type="integer")
+     */
+    protected $currentMana = 0;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="mana", type="integer")
+     */
+    protected $mana = 0;
 
     /**
      * @var integer
      *
      * @ORM\Column(name="level", type="integer")
      */
-    protected $level;
+    protected $level = 1;
 
     /**
      * @param int $currentHp
@@ -194,4 +222,70 @@ abstract class Attributes
     {
         return $this->level;
     }
+
+    /**
+     * @param int $currentMana
+     */
+    public function setCurrentMana($currentMana)
+    {
+        $this->currentMana = $currentMana;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrentMana()
+    {
+        return $this->currentMana;
+    }
+
+    /**
+     * @param int $int
+     */
+    public function setInt($int)
+    {
+        $this->int = $int;
+    }
+
+    /**
+     * @return int
+     */
+    public function getInt()
+    {
+        return $this->int;
+    }
+
+    /**
+     * @param int $mana
+     */
+    public function setMana($mana)
+    {
+        $this->mana = $mana;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMana()
+    {
+        return $this->mana;
+    }
+
+    /**
+     * @param int $spi
+     */
+    public function setSpi($spi)
+    {
+        $this->spi = $spi;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSpi()
+    {
+        return $this->spi;
+    }
+
+
 }
