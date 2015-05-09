f1a2f49d64f31cba733be5c511ca20010b646de1
Joan Madrid <jmadrid@omitsis.com>
2015-05-09 11:44:32 +0200

    upcode.sh added

---
+++ upcode.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+chmod 777 -R app/cache
+chmod 777 -R app/logs
+php composer.phar install
+php app/console cache:clear --no-warmup
+php app/console cache:clear --env=prod --no-warmup
+#echo "y" | php app/console doc:mig:mig
+php app/console doc:sc:up --force
+php app/console assetic:dump
+php app/console assets:install web
+chmod 777 -R app/cache
+chmod 777 -R app/logs
